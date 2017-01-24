6069f5a8c5e6b05e6bb64eb488782974d0f60f32
Ryan Pelton <ryantp92@gmail.com>
2017-01-24 02:47:43 -0800

    files removed || sprite.py updated

--- pygameF/sprite.py
+++ pygameF/sprite.py
@@ -18,6 +18,9 @@ class Char(object):
 	def __str__(self):
 		return "%(n)s x[%(x)d] y[%(y)d]" % {'n': self.name, 'x': self.pos[0], 'y': self.pos[1]}
 
+	def __repr__(self):
+		return "%(n)s x[%(x)d] y[%(y)d]" % {'n': self.name, 'x': self.pos[0], 'y': self.pos[1]}
+
 	def blit(self, img = None):
 		self.screen.blit(img, self.pos)
 
@@ -29,6 +32,20 @@ class Player(Char):
 		self.health = health # [0] = current health; [1] = total health
 		self.pos = pos # [0] = x position; [1] = y position
 		self.screen = screen
+
+	def __str__(self):
+		return "Player %(n)s x[%(x)d] y[%(y)d]" % {
+			'n': self.name,
+			'x': self.pos[0],
+			'y': self.pos[1]
+		}
+
+	def __repr__(self):
+		return "Player %(n)s x[%(x)d] y[%(y)d]" % {
+			'n': self.name,
+			'x': self.pos[0],
+			'y': self.pos[1]
+		}
 
 	def injure(self, damage = 0):
 		# player is injured
@@ -53,6 +70,22 @@ class NPC(Char):
 		self.pos = pos
 		self.screen = screen
 		self.textTool = s.StdTool(screen)
+
+	def __str__(self):
+		return "%(C)s %(n)s x[%(x)d] y[%(y)d]" % {
+			'C': str(NPC).split("'")[1],
+			'n': self.name,
+			'x': self.pos[0],
+			'y': self.pos[1]
+		}
+
+	def __repr__(self):
+		return "%(C)s %(n)s x[%(x)d] y[%(y)d]" % {
+			'C': str(NPC).split("'")[1],
+			'n': self.name,
+			'x': self.pos[0],
+			'y': self.pos[1]
+		}
 
 	def say(self, *args):
 		self.textTool(msg, color, width, height, x_displace = 0, y_displace = 0, fname = None, fsize = 30)
@@ -92,13 +125,21 @@ class Enemy(Char):
 					'y': self.pos[1]
 				}
 		else:
-			s = "Enemy(%(e)s) (UID: %(u)s) -- x[%(x)d] y[%(y)d]" % {
-					'e': Enemy,
+			s = "Enemy(%(C)s) (UID: %(u)s) -- x[%(x)d] y[%(y)d]" % {
+					'C': str(Enemy).split("'")[1],
 					'u': self.uid,
 					'x': self.pos[0],
 					'y': self.pos[1]
 				}
 		return s
+
+	def __repr__(self):
+		return "Enemy(%(C)s) (UID: %(u)s) -- x[%(x)d] y[%(y)d]" % {
+			'C': str(Enemy).split("'")[1],
+			'u': self.uid,
+			'x': self.pos[0],
+			'y': self.pos[1]
+		}
 
 	def chase(self, target = None, speed = 0):
 		# chase specified target; target must be a Char, Player, NPC or Enemy object
@@ -107,4 +148,55 @@ class Enemy(Char):
 
 	def move(self):
 		pass
+
+
+class HealthBar(object):
+	'''standard pygameF health bar'''
+	def __init__(self, health = [], pos = [], screen = None):
+		self.curHealth = health[0] # current health
+		self.ttlHealth = health[1] # total health
+		self.pos = pos # [0]x and [1]y
+		self.screen = screen # pygame screen object
+
+		self.hDim = [16, 16] # 16 X 16 block
+		self.hCol = [250, 0, 0] # rgb value for red
+		self.blitBuff = self.hDim[0] # blit buffer
+
+	def __str__(self):
+		return "HealthBar %(C)s c[%(c)d]/t[%(t)d]" % {
+			'C': str(HealthBar).split("'")[1],
+			'c': self.curHealth,
+			't': self.ttlHealth
+		}
+
+	def __repr__(self):
+		return "HealthBar %(C)s c[%(c)d]/t[%(t)d]" % {
+			'C': str(HealthBar).split("'")[1],
+			'c': self.curHealth,
+			't': self.ttlHealth
+		}
+
+	def get_pos(self):
+		return self.pos
+
+	def set_pos(self, pos = []):
+		self.pos = pos
+
+	def get_hDim(self):
+		return self.hDim
+
+	def set_hDim(self, hDim = []):
+		self.hDim = hDim
+
+	def get_hCol(self):
+		return self.hCol
+
+	def set_hCol(self, hCol = []):
+		self.hCol = hCol
+
+	def blit(self):
+		x = self.pos[0] # initial x blit point
+		for i in range(self.health[0]):
+			pygame.draw.rect(self.screen, self.hCol, [x, self.pos[1], self.hDim[0], self.hDim[1]])
+			x += self.blitBuff
 
